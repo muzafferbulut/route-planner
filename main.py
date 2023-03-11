83828d7b287b9f176bbe96592994a3998e3b6926
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2023-03-11 11:46:13 +0300

    hava  durumu verileri çekildi

---
+++ main.py
@@ -0,0 +1,3 @@
+﻿from scrapper import Site
+import json
+
---
+++ scrapper.py
@@ -0,0 +1,25 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+class Site():
+
+    def __init__(self, site_link):
+        super().__init__()
+        self.site_link = site_link
+
+    def getSiteLink(self):
+        return self.site_link
+
+    def setSiteLink(self, cityName):
+        self.site_link = self.site_link.replace("istanbul", cityName)
+        return self.site_link
+
+    def scrapeData(self):
+        response = requests.get(self.site_link)
+        soup = BeautifulSoup(response.content, "html.parser")
+        table = soup.find("table")
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            for cell in cells:
+                print(cell.text)
