d7b9504884d13bc89bbc915076b60cbc78c7837d
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2023-03-18 21:08:14 +0300

    line verisi harita üzerine eklendi

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ Created on Sat Mar 11 22:12:17 2023
 
 @author: Muzaffer Bulut
 """
-
+import io
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PyQt5.uic import loadUi
-from scrapper import Site
-from PyQt5 import QtWebEngineWidgets
 import folium
+from folium.plugins.draw import Draw
 import geopandas as gpd
-
+from scrapper import Site
+from PyQt5.uic import loadUi
+from PyQt5 import QtWebEngineWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 
 class RoutePlanner(QMainWindow):
     def __init__(self):
@@ -24,15 +24,26 @@ class RoutePlanner(QMainWindow):
         self.getReportButton.accepted.connect(self.generateReport)
         self.getReportButton.rejected.connect(self.close)
         self.horizontalSlider.valueChanged.connect(self.getSliderValue)
-        
-        # folium map
+        self.getMap()
+
+    def getMap(self):
         self.foliumMap = folium.Map(location=[38.52077, 35.85411], zoom_start=6)
+
+        Draw(
+            export=False,
+            position="topleft",
+            draw_options={
+                "polyline": False,
+                "rectangle": False,
+                "circle": False,
+                "circlemarker": False,
+            },
+            edit_options={"poly": {"allowIntersection": False}},
+        ).add_to(self.foliumMap)
         
-        dataShp = gpd.read_file("C:\MyFiles\Kişisel\cekim.shp")
-        
-        folium.GeoJson(dataShp).add_to(self.foliumMap)
-        
-        self.webEngineView.setHtml(self.foliumMap._repr_html_())
+        data = io.BytesIO()
+        self.foliumMap.save(data, close_file=False)
+        self.webEngineView.setHtml(data.getvalue().decode())
 
     def getSliderValue(self):
         self.sliderLabel.setText(str(self.horizontalSlider.value()))
@@ -43,9 +54,18 @@ class RoutePlanner(QMainWindow):
         self.fileDialog.setNameFilter("ESRI Shapefiles (*.shp)")
         self.fileDialog.setFileMode(QFileDialog.ExistingFile)
         self.fileDialog.exec_()
-        self.selectedFile = self.fileDialog.selectedFiles()
-        self.filePathLineEdit.setText(self.selectedFile[0])
+        self.selectedFile = self.fileDialog.selectedFiles()[0]
+        self.filePathLineEdit.setText(self.selectedFile)
+        
+        self.drawRouteOnMap()
 
+    def drawRouteOnMap(self):
+        newRoute = gpd.read_file(self.selectedFile)
+        newRoute.explore(m=self.foliumMap)
+        self.webEngineView.setHtml(self.foliumMap._repr_html_())
+
+
+        
     def exportData(self):
         print("clicked export button.")
 
