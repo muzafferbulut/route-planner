1a3cf315de355c7afe89fb7959f7ade6e99c5443
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2023-03-18 17:24:11 +0300

    slider fonksiyonu eklendi

--- main.py
+++ main.py
@@ -23,11 +23,15 @@ class RoutePlanner(QMainWindow):
         self.exportButton.clicked.connect(self.exportData)
         self.getReportButton.accepted.connect(self.generateReport)
         self.getReportButton.rejected.connect(self.close)
+        self.horizontalSlider.valueChanged.connect(self.getSliderValue)
         
         # folium map
         self.foliumMap = folium.Map(location=[38.52077, 35.85411], zoom_start=6)
         self.webEngineView.setHtml(self.foliumMap._repr_html_())
 
+    def getSliderValue(self):
+        self.sliderLabel.setText(str(self.horizontalSlider.value()))
+    
     def openFile(self):
         self.selectedFile = []
         self.fileDialog = QFileDialog()
