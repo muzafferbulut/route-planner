c94ac5aa207ad49354132859ab0376edbf81e7d1
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2023-03-19 19:49:56 +0300

    simplify ve fitbounds fonksiyonları düzenlendi

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ class RoutePlanner(QMainWindow):
         self.getMap()
 
     def getMap(self):
-        self.foliumMap = folium.Map(location=[38.52077, 35.85411], zoom_start=6)
+        self.foliumMap = folium.Map(location=[38.82, 35.15], zoom_start=6)
 
         Draw(
             export=False,
             position="topleft",
             draw_options={
                 "polyline": True,
-                "rectangle": False,
+                "rectangle": True,
                 "circle": True,
                 "circlemarker": False,
             },
@@ -64,49 +64,35 @@ class RoutePlanner(QMainWindow):
 
     def drawRouteOnMap(self):
         newRoute = gpd.read_file(self.selectedFile)
-        
-        coords = [feature.geometry.coords for _, feature in newRoute.iterrows()]
-        
-        lines = [LineString(coord) for coord in coords]
+        self.simplifyGeom(newRoute)
+        self.fitBounds(newRoute)
+        self.webEngineView.setHtml(self.foliumMap._repr_html_())
 
+    def simplifyGeom(self, newRoute, tolerance=0.00001):
+        coords = [feature.geometry.coords for _, feature in newRoute.iterrows()]
+        lines = [LineString(coord) for coord in coords]
         df = pd.DataFrame({'geometry': lines})
-        tolerance = 0.00001
-        
         simple_coords = [simplify_coords(line.coords, tolerance) for line in newRoute.geometry]
-        
         simple_gdf = gpd.GeoDataFrame(geometry=[LineString(coord) for coord in simple_coords])
-        
         for _, feature in simple_gdf.iterrows():
             folium.PolyLine(locations=feature.geometry.coords).add_to(self.foliumMap)
-        
-        #folium.GeoJson(newRoute).add_to(self.foliumMap)
-        min_lon, min_lat, max_lon, max_lat = newRoute.total_bounds
 
+    def fitBounds(self, newRoute):
+        min_lon, min_lat, max_lon, max_lat = newRoute.total_bounds
         if min_lon == max_lon and min_lat == max_lat:
             # Tüm değerler sıfır ise, harita boyutlarını manuel olarak ayarla
             map_center = [min_lat, min_lon]
             self.foliumMap.setView(location=map_center, zoom_start=10)
         else:
             # Harita boyutlarını otomatik olarak ayarla
-            bounds = [[min_lat, min_lon], [max_lat, max_lon]]
-            
+            bounds = [[min_lat, min_lon], [max_lat, max_lon]] 
         self.foliumMap.fit_bounds(bounds)
-        self.webEngineView.setHtml(self.foliumMap._repr_html_())
         
-    def simplifyGeometries(self):
-        pass
-    
-    def reprojectionGeometries(self):
-        pass
-    
-    def fitBounds(self):
-        pass
-
     def exportData(self):
-        print("clicked export button.")
-
+        print("clicked export data button")
+        
     def generateReport(self):
-        print("clicked get report button.")
+        print("clicked generate report button")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
